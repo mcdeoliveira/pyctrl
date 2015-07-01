1baa5d9c9b45f72cac28b21cbb08f23fa08e331e
Mauricio <mauricio@ucsd.edu>
2015-06-30 18:28:55 -0700

    Added vector and matrix functionality to Packet Added testing for Packet

--- python/Packet.py
+++ python/Packet.py
@@ -1,4 +1,5 @@
 import struct
+import numpy
 
 class Packet:
 
@@ -43,68 +44,63 @@ class Packet:
             return ('D', d)
 
         elif btype == b'V':
+            # Read type (char)
+            buffer = stream.read(1)
+            (vtype,) = struct.unpack('c', buffer)
             # Read length (int)
             buffer = stream.read(4)
-            (blen,) = struct.unpack('<I', buffer)
-            # Read string (blen*char)
-            buffer = stream.read(blen)
-            (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
-            return ('V', str(bmessage, 'utf-8'))
+            (vlen,) = struct.unpack('<I', buffer)
+            # Read data (blen*)
+            if vtype == b'I':
+                vector = numpy.zeros(vlen, int)
+                for k in range(vlen):
+                    buffer = stream.read(4)
+                    (vector[k],) = struct.unpack('<i', buffer)
+            elif vtype == b'F':
+                vector = numpy.zeros(vlen, float)
+                for k in range(vlen):
+                    buffer = stream.read(4)
+                    (vector[k],) = struct.unpack('<f', buffer)
+            elif vtype == b'D':
+                vector = numpy.zeros(vlen, float)
+                for k in range(vlen):
+                    buffer = stream.read(8)
+                    (vector[k],) = struct.unpack('<d', buffer)
+            else:
+                # error
+                pass
+
+            # return vector
+            return ('V', vector)
 
         elif btype == b'M':
-            # Read length (int)
+            # Read row size (int)
             buffer = stream.read(4)
-            (blen,) = struct.unpack('<I', buffer)
-            # Read string (blen*char)
-            buffer = stream.read(blen)
-            (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
-            return ('M', str(bmessage, 'utf-8'))
+            (rsize,) = struct.unpack('<I', buffer)
+            # read vector
+            (vtype, vector) = unpack_stream(stream)
+            # resize vector as matrix
+            vector = numpy.resize(vector, (rsize, vector.size/rsize))
+            # return vector
+            return ('M', vector)
 
         else:
             raise NameError('Unknown type')
 
-    def unpack(buffer):
-
-        (btype,), buffer = struct.unpack('c', buffer[:1]), buffer[1:]
-
-        if btype == b'A':
-            (command,) = struct.unpack('c', buffer[:1])
-            return ('A', str(command, 'utf-8'))
-
-        elif btype == b'C':
-            (command,) = struct.unpack('c', buffer[:1])
-            return ('C', str(command, 'utf-8'))
-
-        elif btype == b'S':
-            (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
-            (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
-            return ('S', str(bmessage, 'utf-8'))
-
-        elif btype == b'I':
-            (i,) = struct.unpack('<i', buffer[:4])
-            return ('I', i)
-
-        elif btype == b'F':
-            (f,) = struct.unpack('<f', buffer[:4])
-            return ('F', f)
-
-        elif btype == b'D':
-            (f,) = struct.unpack('<d', buffer[:8])
-            return ('D', d)
-
-        elif btype == b'V':
-            (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
-            (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
-            return ('V', str(bmessage, 'utf-8'))
-
-        elif btype == b'M':
-            (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
-            (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
-            return ('M', str(bmessage, 'utf-8'))
-
+    def pack_vector(type, content):
+        buffer = b''
+        type = '<' + type
+        if len(content.shape) > 1:
+            # matrix
+            for k in range(content.shape[1]):
+                for value in content[:,k]:
+                    buffer += struct.pack(type, value)
         else:
-            raise NameError('Unknown type')
-
+            # vector
+            for value in content:
+                buffer += struct.pack(type, value)
+        return buffer
+        
     def pack(type, content):
 
         # command
@@ -139,21 +135,67 @@ class Packet:
 
         #vector
         elif type == 'V':
-            bmessage = bytes(content, 'utf-8')
-            blen = len(bmessage)
-            return struct.pack('<cI%ds' % (blen,),
-                               b'V', 
-                               blen,
-                               bmessage)
+            vlen = content.size
+            if numpy.issubsctype(content, numpy.int):
+                return ( struct.pack('<ccI', b'V', b'I', vlen) +
+                         Packet.pack_vector('i', content) )
+            elif numpy.issubsctype(content, numpy.float32):
+                return ( struct.pack('<ccI', b'V', b'F', vlen) +
+                         Packet.pack_vector('f', content) )
+            elif numpy.issubsctype(content, numpy.float):
+                return ( struct.pack('<ccI', b'V', b'D', vlen) +
+                         Packet.pack_vector('d', content) )
+            else:
+                # error
+                pass
 
         #matrix
         elif type == 'M':
-            bmessage = bytes(content, 'utf-8')
-            blen = len(bmessage)
-            return struct.pack('<cI%ds' % (blen,),
-                               b'M', 
-                               blen,
-                               bmessage)
+            rsize = content.shape[0]
+            return ( struct.pack('<cI', b'M', rsize) +
+                     Packet.pack('V', content) )
 
         else:
             raise NameError('Unknown type')
+
+    # def unpack(buffer):
+
+    #     (btype,), buffer = struct.unpack('c', buffer[:1]), buffer[1:]
+
+    #     if btype == b'A':
+    #         (command,) = struct.unpack('c', buffer[:1])
+    #         return ('A', str(command, 'utf-8'))
+
+    #     elif btype == b'C':
+    #         (command,) = struct.unpack('c', buffer[:1])
+    #         return ('C', str(command, 'utf-8'))
+
+    #     elif btype == b'S':
+    #         (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
+    #         (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
+    #         return ('S', str(bmessage, 'utf-8'))
+
+    #     elif btype == b'I':
+    #         (i,) = struct.unpack('<i', buffer[:4])
+    #         return ('I', i)
+
+    #     elif btype == b'F':
+    #         (f,) = struct.unpack('<f', buffer[:4])
+    #         return ('F', f)
+
+    #     elif btype == b'D':
+    #         (f,) = struct.unpack('<d', buffer[:8])
+    #         return ('D', d)
+
+    #     elif btype == b'V':
+    #         (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
+    #         (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
+    #         return ('V', str(bmessage, 'utf-8'))
+
+    #     elif btype == b'M':
+    #         (blen,), buffer = struct.unpack('<I', buffer[:4]), buffer[4:]
+    #         (bmessage,) = struct.unpack('%ds' % (blen,), buffer)
+    #         return ('M', str(bmessage, 'utf-8'))
+
+    #     else:
+    #         raise NameError('Unknown type')
