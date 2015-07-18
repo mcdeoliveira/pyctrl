4215a9bfab174d2a1860bbc4485e734d0545bedf
root <root@beaglebone.localdomain>
2015-07-18 01:19:14 +0000

    Tested controllers in ControllerBBB

--- python/ControllerBBB.py
+++ python/ControllerBBB.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
     Ts = 0.01              # s
     a = 17                 # 1/s
-    k = 1.63               # cycles/s duty
+    k = 0.163               # cycles/s duty
 
     controller = ControllerBBB(Ts, 1)
     controller.set_logger(2)
@@ -94,11 +94,13 @@ if __name__ == "__main__":
     controller.start()
     time.sleep(1)
     controller.set_reference1(100)
-    time.sleep(5)
+    time.sleep(2)
     controller.set_reference1(50)
-    time.sleep(5)
+    time.sleep(2)
     controller.set_reference1(-50)
-    time.sleep(5)
+    time.sleep(2)
+    controller.set_reference1(-100)
+    time.sleep(2)
     controller.set_reference1(0)
     time.sleep(1)
     controller.stop()
@@ -106,17 +108,49 @@ if __name__ == "__main__":
     print('> OPEN LOOP CONTROL (POTENTIOMETER)')
     controller.set_reference1_mode(1)
     controller.start()
-    time.sleep(30)
+    time.sleep(2)
     controller.stop()
 
-    reference = 2
+    reference = 6
     print('> CLOSED LOOP CONTROL (POSITION, REFERENCE = {})'.format(reference))
     controller.set_reference1_mode(0)
     controller.set_controller1(
-        ProportionalController(0.09 / (k*Ts), reference / 100)
+        ProportionalController(30 / k, reference / 100)
     )
     controller.start()
     time.sleep(1)
     controller.set_reference1(100)
+    time.sleep(2)
+    controller.stop()
+
+    print('> CLOSED LOOP CONTROL (POSITION, POTENTIOMETER)')
+    controller.set_reference1_mode(1)
+    controller.set_controller1(
+        ProportionalController(30 / k, reference / 100)
+    )
+    controller.start()
     time.sleep(5)
     controller.stop()
+
+    reference = 11
+
+    controller.set_controller1(
+        VelocityController(ProportionalController(5 / k, reference / 100))
+    )
+
+    print('> CLOSED LOOP ON VELOCITY')
+    controller.set_reference1_mode(0)
+    controller.start()
+    time.sleep(1)
+    controller.set_reference1(100)
+    print('\n> REFERENCE = {}'.format(reference))
+    time.sleep(3)
+    controller.set_reference1(50)
+    print('\n> REFERENCE = {}'.format(reference/2))
+    time.sleep(3)
+    controller.set_reference1(-50)
+    print('\n> REFERENCE = {}'.format(-reference/2))
+    time.sleep(3)
+    controller.stop()
+
+
