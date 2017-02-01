e41259aafb39fc45119a5077d67c69cb9096d456
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-02-01 15:13:35 -0800

    mip3

--- python/ctrl/__init__.py
+++ python/ctrl/__init__.py
@@ -371,8 +371,6 @@ class Controller:
 
         try:
 
-            print('kwargs = {}'.format(kwargs))
-            
             # create device
             obj_class = getattr(importlib.import_module(device_module), 
                                 device_class)
