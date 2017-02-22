a4b98eaaee01d3099daaf0cb0ed58ff7ddd3a9fd
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-02-21 17:59:47 -0800

    Workaround for setting Ts

--- python/ctrl/__init__.py
+++ python/ctrl/__init__.py
@@ -371,6 +371,8 @@ class Controller:
 
         try:
 
+            print("ctrl.add_device: KWARGS = {}".format(kwargs))
+            
             # create device
             obj_class = getattr(importlib.import_module(device_module), 
                                 device_class)
