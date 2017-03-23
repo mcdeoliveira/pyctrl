75fbd7b47c4687e87072a737ebee8b7df905d772
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-03-22 17:27:46 -0700

    More docs and bbb+rc port

--- python/ctrl/rc/__init__.py
+++ python/ctrl/rc/__init__.py
@@ -14,10 +14,10 @@ class Controller(ctrl.Controller):
         super().__init__(*vargs, **kwargs)
 
         # set state as RUNNING
-        rcpy.set_state(rc.RUNNING)
+        rcpy.set_state(rcpy.RUNNING)
 
         # register cleanup function
-        rcpy.add_cleanup(rcpy.set_state, (ctrl.EXITING,))
+        rcpy.add_cleanup(rcpy.set_state, (rcpy.EXITING,))
 
     def __reset(self):
 
