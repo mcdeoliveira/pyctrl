380a1d703ced55b45a82480d8ab1ba21081dea13
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-06-25 16:02:00 -0700

    Added rc/led and changed add_device to use BlockType

--- pyctrl/rc/mip.py
+++ pyctrl/rc/mip.py
@@ -1,6 +1,7 @@
 import rcpy
 import pyctrl
 import pyctrl.rc
+from pyctrl import BlockType
 
 class Controller(pyctrl.rc.Controller):
 
@@ -16,14 +17,14 @@ class Controller(pyctrl.rc.Controller):
         # add source: imu
         self.add_device('inclinometer',
                         'pyctrl.rc.mpu9250', 'Inclinometer',
-                        type = 'source',
+                        type = BlockType.SOURCE,
                         enable = False,
                         outputs = ['theta','theta_dot'])
 
         # add source: encoder1
         self.add_device('encoder1',
                         'pyctrl.rc.encoder', 'Encoder',
-                        type = 'source',
+                        type = BlockType.SOURCE,
                         outputs = ['encoder1'],
                         encoder = 3, 
                         ratio = 60 * 35.557)
@@ -31,7 +32,7 @@ class Controller(pyctrl.rc.Controller):
         # add source: encoder2
         self.add_device('encoder2',
                         'pyctrl.rc.encoder', 'Encoder',
-                        type = 'source',
+                        type = BlockType.SOURCE,
                         outputs = ['encoder2'],
                         encoder = 2, 
                         ratio = - 60 * 35.557)
@@ -39,7 +40,7 @@ class Controller(pyctrl.rc.Controller):
         # add sink: motor1
         self.add_device('motor1', 
                         'pyctrl.rc.motor', 'Motor',
-                        type = 'sink',
+                        type = BlockType.SINK,
                         enable = True,
                         inputs = ['pwm1'],
                         motor = 3)
@@ -47,7 +48,7 @@ class Controller(pyctrl.rc.Controller):
         # add sink: motor2
         self.add_device('motor2', 
                         'pyctrl.rc.motor', 'Motor',
-                        type = 'sink',
+                        type = BlockType.SINK,
                         enable = True,
                         inputs = ['pwm2'],
                         motor = 2,
