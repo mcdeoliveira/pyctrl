9db4fe3c7405a14f34f6a13fe6317706160a853f
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-06-30 17:56:37 -0700

    Small fixes to rc examples

--- examples/rc_mip_balance.py
+++ examples/rc_mip_balance.py
@@ -176,8 +176,8 @@ def main():
     
     # add small angle sensor
     mip.add_filter('timer/supervisor/is_angle_small',
-                   CompareAbsWithHysterisis(threshold = 0.135,
-                                            hysterisis = 0.115,
+                   CompareAbsWithHysterisis(threshold = 0.11,
+                                            hysterisis = 0.09,
                                             offset = -0.07,
                                             state = (State.LOW,)),
                    ['theta'],
