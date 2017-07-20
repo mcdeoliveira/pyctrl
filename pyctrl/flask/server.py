1e01e58ba183a8048927bb5ff43e488c33fc4a20
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-07-19 22:35:58 -0700

    rc_mip_balance

--- pyctrl/flask/server.py
+++ pyctrl/flask/server.py
@@ -5,6 +5,7 @@ import re
 import pyctrl
 import warnings
 import importlib
+import traceback, sys
 
 from pyctrl.flask import JSONEncoder, JSONDecoder
 
@@ -56,12 +57,20 @@ def json_response(f):
             if retval is None:
                 retval = { 'status': 'success' }
         except Exception as e:
-            retval = { 'status': 'error', 'message': repr(e) }
-
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            retval = { 'status': 'error',
+                       'filename': exc_traceback.tb_frame.f_code.co_filename,
+                       'lineno'  : exc_traceback.tb_lineno,
+                       'name'    : exc_traceback.tb_frame.f_code.co_name,
+                       'type'    : exc_type.__name__,
+                       'message' : exc_value.message }
+            
         next = request.args.get('next', None)
         if next:
             if 'status' in retval and retval['status'] == 'error':
-                flash('Error: ' + retval['message'])
+                flash('Error in file "{}", line {}, in {} {}: {}'.format(
+                    retval['filename'], retval['lineno'], retval['name'],
+                    retval['type'], retval['message'])
             return redirect(url_for(next))
         else:
             return jsonify(retval)
